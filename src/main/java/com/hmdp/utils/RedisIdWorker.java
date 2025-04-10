c4d77984322cb0cc0d935549a5dbfd04a4fd0069
25188 <2518880465@qq.com>
2025-04-10 11:10:11 +0800

    全局唯一id生成器

---
+++ src/main/java/com/hmdp/utils/RedisIdWorker.java
@@ -0,0 +1,64 @@
+package com.hmdp.utils;
+
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
+/*
+基于redis实现的全局id生成器
+ */
+@Component
+public class RedisIdWorker {
+    /*
+        stringRedisTemplate的工具类
+         */
+    private StringRedisTemplate stringRedisTemplate;
+
+    /*
+    起始时间戳
+     */
+    private static final long BEGIN_TIMESTAMP = 1640995200L;
+
+    private static final int BIT_SHIFT = 32;
+
+    public RedisIdWorker(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
+    /*
+    根据前缀来创建全局id
+    */
+    public long createId(String keyPrefix) {
+        //1.生成时间戳
+        LocalDateTime now = LocalDateTime.now();
+        long nowEpochSecond = now.toEpochSecond(ZoneOffset.UTC);
+        long timestamp = nowEpochSecond - BEGIN_TIMESTAMP;
+
+        //2.生成序列号
+        //2.1生成当前时间
+        LocalDate nowDay = LocalDate.now();
+        //2.2自定义日期格式
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy:MM:dd");
+        //2.3格式化为指定形式
+        String format = formatter.format(nowDay);
+
+        long number = stringRedisTemplate.opsForValue().increment("icr:" + keyPrefix + ":" + format);
+
+
+        //3.拼接返回
+        return timestamp << BIT_SHIFT | number;
+    }
+
+
+//    public static void main(String[] args) {
+//        LocalDateTime localDateTime = LocalDateTime.of(2022, 1, 1, 0, 0, 0);
+//        long l = localDateTime.toEpochSecond(ZoneOffset.UTC);
+//        System.out.println("l="+l);
+//    }
+}
--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,18 +1,36 @@
 package com.hmdp;
 
 import com.hmdp.service.impl.ShopServiceImpl;
+import com.hmdp.utils.RedisIdWorker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
     @Resource
     private ShopServiceImpl shopServiceImpl;
+    @Resource
+    private RedisIdWorker redisIdWorker;
+    private static final ExecutorService executorService = Executors.newFixedThreadPool(100);
 
     @Test
     void testInsertHotKey(){
         shopServiceImpl.setShopHotKey(1L,1L);
+    }
+
+    @Test
+    void testRedisIdWork(){
+        Runnable runnable = () -> {
+            for (int i = 0; i < 100; i++) {
+                long id = redisIdWorker.createId("test");
+                System.out.println(id);
+            }
+        };
+
+        executorService.execute(runnable);
     }
 }
